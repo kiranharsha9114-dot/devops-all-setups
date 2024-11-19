2ef0c7ca91be999e39182874d397066adb974028
Dinesh-Amaranadh <doddaka.dinesh@gmail.com>
2024-11-19 14:13:49 +0530

    Update jenkins.sh
    Updated java 17 version command by commenting the java 11 version command to set up Jenkins as per latest update

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,9 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #STEP-3: DOWNLOAD JAVA11 AND JENKINS
-amazon-linux-extras install java-openjdk11 -y
+# amazon-linux-extras install java-openjdk11 -y ( this comand no more needed to install jenkins we need java 17 version)
+# To install java 17 version
+yum install java-17-amazon-corretto -y
 yum install jenkins -y
 update-alternatives --config java
 
