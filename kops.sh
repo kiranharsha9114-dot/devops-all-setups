6072ae654680c9d1ff36b368722b49d83fa8edef
Srujan28 <66787265+Srujan28@users.noreply.github.com>
2025-05-21 11:21:50 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -15,8 +15,8 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-aws s3api create-bucket --bucket srujan.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket srujan.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://srujan.k8s.local
+aws s3api create-bucket --bucket srujans3.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket srujans3.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://srujans3.k8s.local
 kops create cluster --name srujancluster.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name srujancluster.k8s.local --yes --admin
