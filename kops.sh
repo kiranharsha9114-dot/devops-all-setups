0f15c38af7be98fe11e028e6068b0741c703bc8a
bharathpolanki <bharathkumarpolanki745@gmail.com>
2025-06-04 11:56:05 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -13,6 +13,6 @@ mv kops-linux-amd64 /usr/local/bin/kops
 
 aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+export KOPS_STATE_STORE=s3://bharath.bucket.k8s
+kops create cluster --name bharath.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --master-volume-size 28 --node-size t2.micro --node-count 2 --node-volume-size 20
+kops update cluster --name bharath.k8s.local --yes --admin
